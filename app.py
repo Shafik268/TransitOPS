2926c989dd91b848c45af935aa86b123d52b211d
Heetjain1802 <heetljain2008@gmail.com>
2026-07-12 13:03:12 +0530

    Modified app

--- app.py
+++ app.py
@@ -16,10 +16,6 @@ with app.app_context():
     # Import routes inside app_context so they bind to app cleanly without duplication
     import routes
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a7658d3dbb9317701c4ef6f0512e13561e6b9e02
 if __name__ == '__main__':
     # Initialize framework execution loop on default port 5000
     app.run(host='0.0.0.0', port=5000, debug=True)
